c09a356c7e730cb5657e39dd96cab371bd02dbf0
bikramgiri <asgbrg.github@gmail.com>
2025-03-04 00:22:03 +0545

    learn about async thunks

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+// import React from 'react'
+import { useDispatch } from 'react-redux'
+import { register } from '../../../store/authSlice'
 import Form from './components/Form/Form'
 
 const Register = () => {
+  const dispatch = useDispatch()
+  const handleRegister = (data) => {
+	dispatch(register(data))
+  }
   return (
-	<Form type="Register"/>
+	<Form type="Register" onSubmit={handleRegister}/>
   )
 }
 
--- src/pages/auth/components/Form/Form.jsx
+++ src/pages/auth/components/Form/Form.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
-const Form = ({type}) => {
+const Form = ({type,onSubmit}) => {
+	const [data,setData] = useState({
+		email : '',
+		username : '',
+		password : ''
+	})
+	const handleChange = (e) => {
+		const {name,value} = e.target
+		setData({
+			...data,
+			[name] : value
+		})
+	}
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		onSubmit(data)
+	}
   return (
       <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
 	<div className="relative py-3 sm:max-w-xl sm:mx-auto">
@@ -14,23 +32,24 @@ const Form = ({type}) => {
                                     {type === 'Login' ? 'Login here to continue...' : 'Register here to continue...'}
                                     </h1>
 				</div>
-				<div className="divide-y divide-gray-200">
+				<form onSubmit={handleSubmit}>
+			      <div className="divide-y divide-gray-200">
 					<div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
 						<div className="relative">
-							<input autoComplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
+							<input autoComplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" onChange={handleChange} required />
 							<label htmlFor="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
 						</div>
                                     {
                                           type === "Register" && (
                                                 <div className="relative">
-							      <input autoComplete="off" id="username" name="username" type="username" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="username" />
+							      <input autoComplete="off" id="username" name="username" type="username" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="username" onChange={handleChange} required />
 							      <label htmlFor="username" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Username</label>
 						            </div>
                                           )
                                     }
                                     
 						<div className="relative">
-							<input autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
+							<input autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" onChange={handleChange} required />
 							<label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
 						</div>
 						<div className="relative">
@@ -38,11 +57,25 @@ const Form = ({type}) => {
 						</div>
 					</div>
 				</div>
+				{
+					type === "Register" ? (
+						<Link to="/login" style={{color:"blue"}}>Go to Login</Link>
+					) : (
+						<Link to="/register" style={{color:"blue"}}>Go to Register</Link>
+					)
+				}	
+				</form>
 			</div>
 		</div>
 	</div>
 </div>
   )
 }
+
+// Props Validation
+Form.propTypes = {
+	type: PropTypes.string.isRequired,
+	onSubmit: PropTypes.func.isRequired,
+    }
 
 export default Form
